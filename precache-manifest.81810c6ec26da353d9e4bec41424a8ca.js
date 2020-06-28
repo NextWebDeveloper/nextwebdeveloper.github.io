fb53b4bb248cbd7408dc70b1d8701c4b8fdd3809
dmitrii.toshchev <dmitrii.toshchev@smartmail.biz>
2020-06-28 19:28:07 +0300

    init repository

---
+++ precache-manifest.81810c6ec26da353d9e4bec41424a8ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aa8dc63c4f803cf664b751cb032e53b",
+    "url": "./static/media/loader.2aa8dc63.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "02f4ebdfe095f73031fa",
+    "url": "./static/js/main.a6de706e.chunk.js"
+  },
+  {
+    "revision": "3df682c2cb8ef929f17f",
+    "url": "./static/js/2.e2083a92.chunk.js"
+  },
+  {
+    "revision": "02f4ebdfe095f73031fa",
+    "url": "./static/css/main.7664cc84.chunk.css"
+  },
+  {
+    "revision": "3df682c2cb8ef929f17f",
+    "url": "./static/css/2.7b9cc7e0.chunk.css"
+  },
+  {
+    "revision": "b8e58f57fb839313d7f936f8ddc1c96d",
+    "url": "./index.html"
+  }
+];
